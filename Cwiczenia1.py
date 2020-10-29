378069b637af344b43016185741e9276574de476
pawel9009 <pawel.b512222@gmail.com>
2020-10-29 12:18:31 +0100

    zadanie4

--- Cwiczenia1.py
+++ Cwiczenia1.py
@@ -1,14 +1,5 @@
-text = '''
-Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym.
-Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. 
-Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. 
-Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem 
-Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji 
-druków na komputerach osobistych, jak Aldus PageMaker
-'''
+napis = "Witaj swiecie"
 
-imie = "Pawel"
-nazwisko = "Budzinski"
+print(dir(napis))
+help(napis.capitalize())
 
-print("W texcie jest ",text.count(imie[2]), " liter ", imie[2],
-      "oraz ", text.count(nazwisko[3]), " lieter ", nazwisko[3] )
---
+++ formatowanie.py
@@ -0,0 +1,8 @@
+text = "Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym." \
+       " Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej " \
+       "książki. "
+
+print(text)
+
+a = parse("wsd", )
+
