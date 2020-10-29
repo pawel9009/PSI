c5115d4f22b6b2c808ebc58d2bf0f03a5fd4c32e
pawel9009 <pawel.b512222@gmail.com>
2020-10-29 12:47:16 +0100

    zadanie8

--- Cwiczenia1.py
+++ Cwiczenia1.py
@@ -1,16 +1,2 @@
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-lista2 = lista[5:]
-del lista[5:]
-
-print(lista)
-print(lista2)
-
-suma = lista + lista2
-suma.insert(0, 0)
-print(suma)
-posortowana = suma
-print(posortowana[::-1])
-
-
+krotka = (150509, 158531, "Janusz nowak", "Budzinski pawel")
 
