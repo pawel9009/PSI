9354420acdb394150dc38d22f141324afd957d87
pawel9009 <pawel.b512222@gmail.com>
2020-11-05 12:29:04 +0100

    zadanie5

---
+++ lab2_zadanie5.py
@@ -0,0 +1,13 @@
+class Calculator:
+    def add(x, y):
+        return x + y
+
+    def difference(x, y):
+        return x - y
+
+    def multiply(x, y):
+        return x * y
+
+    def divide(x, y):
+        if y != 0:
+            return x / y
